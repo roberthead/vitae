366330c6341788cba4ddc6d41f08d40a7e81dc97
Rob Head <robert.head@gmail.com>
2015-11-11 06:24:54 -0800

    Configure the generators

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,15 @@ Bundler.require(*Rails.groups)
 
 module Vitae
   class Application < Rails::Application
+    
+  config.generators do |g|
+    g.orm             :active_record
+    g.template_engine :haml
+    g.test_framework  :rspec, fixture: false
+    g.stylesheets     false
+    g.javascripts     false
+  end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
