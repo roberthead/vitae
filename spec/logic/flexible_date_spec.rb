ccc38c0e97588277d64a16134b35a60fa3b542fc
Rob Head <robert.head@gmail.com>
2015-11-26 15:57:06 -0800

    More expectations for FlexibleDate spec.

--- spec/logic/flexible_date_spec.rb
+++ spec/logic/flexible_date_spec.rb
@@ -5,6 +5,23 @@ describe FlexibleDate do
     let(:flex_date) { FlexibleDate.new(2005) }
 
     specify { expect(flex_date.to_db).to eq "2005" }
+
     specify { expect(flex_date.date_range).to eq (Date.new(2005, 1, 1)..Date.new(2005, 12, 31)) }
+  end
+
+  context "with a year and a month" do
+    let(:flex_date) { FlexibleDate.new(2005, 12) }
+
+    specify { expect(flex_date.to_db).to eq "2005-12" }
+
+    specify { expect(flex_date.date_range).to eq (Date.new(2005, 12, 1)..Date.new(2005, 12, 31)) }
+  end
+
+  context "with a year and a month" do
+    let(:flex_date) { FlexibleDate.new(2005, 12, 26) }
+
+    specify { expect(flex_date.to_db).to eq "2005-12-26" }
+
+    specify { expect(flex_date.date_range).to eq (Date.new(2005, 12, 26)..Date.new(2005, 12, 26)) }
   end
 end
