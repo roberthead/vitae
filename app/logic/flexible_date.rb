8e53e207bef29ae1179da3ebd2612c658a4c36db
Rob Head <robert.head@gmail.com>
2015-11-26 16:13:12 -0800

    Add short and long string versions of dates.

--- app/logic/flexible_date.rb
+++ app/logic/flexible_date.rb
@@ -1,10 +1,22 @@
 class FlexibleDate < Struct.new(:year, :month, :day)
+  MONTHS = %w{January February March April May June July August September October November December}
+
   def to_db
     [year, month, day].select(&:present?).join('-')
   end
 
   def date_range
     date_range_start..date_range_end
+  end
+
+  def to_s(options = {})
+    if day
+      "#{month_string(options[:month])} #{day}, #{year}"
+    elsif month
+      "#{month_string(options[:month])} #{year}"
+    else
+      year.to_s
+    end
   end
 
   private
@@ -27,5 +39,19 @@ class FlexibleDate < Struct.new(:year, :month, :day)
     elsif year
       Date.new(year).end_of_year
     end
+  end
+
+  def month_string(format = nil)
+    if month
+      format.to_s =~ /short/i ? month_short_string : month_long_string
+    end
+  end
+
+  def month_long_string
+    MONTHS[month - 1]
+  end
+
+  def month_short_string
+    MONTHS[month - 1][0..2]
   end
 end
--- spec/logic/flexible_date_spec.rb
+++ spec/logic/flexible_date_spec.rb
@@ -7,6 +7,8 @@ describe FlexibleDate do
     specify { expect(flex_date.to_db).to eq "2005" }
 
     specify { expect(flex_date.date_range).to eq (Date.new(2005, 1, 1)..Date.new(2005, 12, 31)) }
+
+    specify { expect(flex_date.to_s).to eq "2005" }
   end
 
   context "with a year and a month" do
@@ -15,6 +17,10 @@ describe FlexibleDate do
     specify { expect(flex_date.to_db).to eq "2005-12" }
 
     specify { expect(flex_date.date_range).to eq (Date.new(2005, 12, 1)..Date.new(2005, 12, 31)) }
+
+    specify { expect(flex_date.to_s).to eq "December 2005" }
+
+    specify { expect(flex_date.to_s(month: :short)).to eq "Dec 2005" }
   end
 
   context "with a year and a month" do
@@ -23,5 +29,7 @@ describe FlexibleDate do
     specify { expect(flex_date.to_db).to eq "2005-12-26" }
 
     specify { expect(flex_date.date_range).to eq (Date.new(2005, 12, 26)..Date.new(2005, 12, 26)) }
+
+    specify { expect(flex_date.to_s).to eq "December 26, 2005" }
   end
 end
