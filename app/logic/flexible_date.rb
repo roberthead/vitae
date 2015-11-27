205584030d0cb23776d018a49858a6b2322033a0
Rob Head <robert.head@gmail.com>
2015-11-26 16:45:43 -0800

    Interpret seasons and month strings.

--- app/logic/flexible_date.rb
+++ app/logic/flexible_date.rb
@@ -1,8 +1,21 @@
-class FlexibleDate < Struct.new(:year, :month, :day)
+class FlexibleDate < Struct.new(:year, :month_or_season, :day)
   MONTHS = %w{January February March April May June July August September October November December}
+  ABBREVIATED_MONTHS = MONTHS.map { |month| month[0..2] }
+  SEASONS = %w{Winter Spring Summer Fall}
+
+  attr_accessor :month, :season
+
+  def initialize(*)
+    super
+    populate_month_or_season
+  end
 
   def to_db
-    [year, month, day].select(&:present?).join('-')
+    if season
+      [year, season].join('-')
+    else
+      [year, month, day].select(&:present?).join('-')
+    end
   end
 
   def date_range
@@ -14,9 +27,15 @@ class FlexibleDate < Struct.new(:year, :month, :day)
       "#{month_string(options[:month])} #{day}, #{year}"
     elsif month
       "#{month_string(options[:month])} #{year}"
+    elsif season
+      "#{season} #{year}"
     else
       year.to_s
     end
+  end
+
+  def season?
+    season.present?
   end
 
   private
@@ -24,6 +43,8 @@ class FlexibleDate < Struct.new(:year, :month, :day)
   def date_range_start
     if day
       Date.new(year, month, day)
+    elsif season
+      Date.new(year, SEASONS.index(season) * 3 + 1)
     elsif month
       Date.new(year, month)
     elsif year
@@ -34,6 +55,8 @@ class FlexibleDate < Struct.new(:year, :month, :day)
   def date_range_end
     if day
       Date.new(year, month, day)
+    elsif season
+      Date.new(year, SEASONS.index(season) * 3 + 3).end_of_month
     elsif month
       Date.new(year, month).end_of_month
     elsif year
@@ -53,5 +76,21 @@ class FlexibleDate < Struct.new(:year, :month, :day)
 
   def month_short_string
     MONTHS[month - 1][0..2]
+  end
+
+  def populate_month_or_season
+    case month_or_season
+    when String, Symbol
+      self.month_or_season = self.month_or_season.to_s.titlecase
+      if MONTHS.include? month_or_season
+        self.month = MONTHS.index(month_or_season) + 1
+      elsif ABBREVIATED_MONTHS.include? month_or_season
+        self.month = ABBREVIATED_MONTHS.index(month_or_season) + 1
+      elsif SEASONS.include?(month_or_season)
+        self.season = month_or_season
+      end
+    when Integer
+      self.month = month_or_season
+    end
   end
 end
--- spec/logic/flexible_date_spec.rb
+++ spec/logic/flexible_date_spec.rb
@@ -11,8 +11,20 @@ describe FlexibleDate do
     specify { expect(flex_date.to_s).to eq "2005" }
   end
 
-  context "with a year and a month" do
+  context "with a year and a numerical month" do
     let(:flex_date) { FlexibleDate.new(2005, 12) }
+
+    specify { expect(flex_date.to_db).to eq "2005-12" }
+
+    specify { expect(flex_date.date_range).to eq (Date.new(2005, 12, 1)..Date.new(2005, 12, 31)) }
+
+    specify { expect(flex_date.to_s).to eq "December 2005" }
+
+    specify { expect(flex_date.to_s(month: :short)).to eq "Dec 2005" }
+  end
+
+  context "with a year and an english month" do
+    let(:flex_date) { FlexibleDate.new(2005, :december) }
 
     specify { expect(flex_date.to_db).to eq "2005-12" }
 
@@ -31,5 +43,15 @@ describe FlexibleDate do
     specify { expect(flex_date.date_range).to eq (Date.new(2005, 12, 26)..Date.new(2005, 12, 26)) }
 
     specify { expect(flex_date.to_s).to eq "December 26, 2005" }
+  end
+
+  context "with a year and a quarter" do
+    let(:flex_date) { FlexibleDate.new(2005, "Fall") }
+
+    specify { expect(flex_date.to_db).to eq "2005-Fall" }
+
+    specify { expect(flex_date.date_range).to eq (Date.new(2005, 10, 1)..Date.new(2005, 12, 31)) }
+
+    specify { expect(flex_date.to_s).to eq "Fall 2005" }
   end
 end
