32c023e6efd416bcd76f8b1e8a3064cbdd80381f
Rob Head <robert.head@gmail.com>
2015-11-28 17:16:21 -0800

    fix spec

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :work do
     title { Faker::Lorem.words([1,2,3,4,5].sample) }
     subtitle { [nil, Faker::Lorem.words([1,2,3,4,5].sample)].sample }
-    attribution "Robert Emerson Head"
     creation_flexdate "2015"
   end
 end
