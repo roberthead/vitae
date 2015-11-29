f38d9e51532613779eb04849c65331dda82204d5
Rob Head <robert.head@gmail.com>
2015-11-28 17:40:12 -0800

    Fix bug in Attribution factory

--- spec/factories/attributions.rb
+++ spec/factories/attributions.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :attribution do
     name_last { Faker::Name.last_name }
     name_first { Faker::Name.first_name }
-    name_middle { [nil, Faker::Name.middle_name].sample }
+    name_middle { [nil, Faker::Name.first_name].sample }
     citation
     editor { [false, true].sample }
   end
